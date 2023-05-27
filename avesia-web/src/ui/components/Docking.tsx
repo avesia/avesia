a3714c77bcf49ed7d1e32a1e898963d1be63e3e3
3k4 <doraidamon1rwfo@outlook.jp>
2023-05-28 08:53:12 +0900

    [web] Add `Export` button.

--- avesia-web/src/ui/components/Docking.tsx
+++ avesia-web/src/ui/components/Docking.tsx
@@ -1,6 +1,32 @@
-export default function Docking() {
+import { useEffect } from "react";
+
+export interface Window {
+    windowId: String,
+    title: String,
+    dockedAreaId: String,
+    element: React.ReactNode,
+}
+
+export interface DockingArea {
+    areaId: String,
+}
+
+interface WindowManagerProps {
+    areas: Array<DockingArea>,
+    windows: Array<Window>,
+}
+
+export default function WindowManager(props: WindowManagerProps) {
+    props;
+    useEffect(() => {
+        
+    });
+
     return (
         <>
+            <div className="window-area">
+                
+            </div>
         </>
-    )
+    );
 }
--- avesia-web/src/ui/components/QuickActionBar.tsx
+++ avesia-web/src/ui/components/QuickActionBar.tsx
@@ -1,9 +1,15 @@
-import { Dropdown, Play } from "../icons/icons";
+import { Dropdown, Play, Share } from "../icons/icons";
 
 export default function QuickActionBar() {
     return (
         <>
             <div className="quickactionbar">
+                <button title="Export" className="button">
+                        <div className="wrap-blockicon">
+                            <Share />
+                        </div>
+                        <p>Export</p>
+                </button>
                 <div className="button-group">
                     <button title="Preview" className="button primary">
                         <div className="wrap-blockicon">
